8641d942d739506482490b7adc62bba56c1041e9
Marina <123579383+demon-for-arcangel@users.noreply.github.com>
2023-09-21 18:09:57 +0200

    Actualización del proyecto

--- Desarrollo en entorno Cliente/15-09-2023/app.js
+++ Desarrollo en entorno Cliente/15-09-2023/app.js
@@ -13,7 +13,7 @@ function acceder(){
     var msg = ''
     msg = validarLogin()
 
-    if (msg = ''){
+    if (msg === ''){
         if (email == persona.correo && password == persona.pwd){
             window.location.href = 'pagina.html'
         }else{
@@ -26,64 +26,85 @@ function acceder(){
 
 function limpiar(){
     document.getElementById('nombre').value = ''
-    document.getElementById('apellido').value = ''
+    document.getElementById('apellido1').value = ''
+    document.getElementById('apellido2').value = ''
+    document.getElementById('dni').value = ''
 }
 
 function guardar(){
-    //var validado = 
+    var validado = validarDatos()
+
     if (validado){
-        //Guardar los datos en el objeto persona
+        // Guardar los datos en el objeto persona
         persona.nombre = document.getElementById('nombre').value
-        persona.apellido = document.getElementById('apellido').value 
+        persona.apellido = document.getElementById('apellido1').value 
+        persona.apellido2 = document.getElementById('apellido2').value 
         persona.dni = document.getElementById('dni').value
-        persona.asignaturas = document.getElementById('asignaturas').value
 
         alert('Datos guardados correctamente.')
     }
 }
 
 function validarLogin(){
-    var correo = document.getElementById('correo').value
-    var contrasena = document.getElementById('constrasena').value
-    var msg = ''
-    msg = validarEmail()
+    var correo = document.querySelector('input[type="email"]').value
+    var contrasena = document.querySelector('input[type="password"]').value
+    var msg = validarEmail()
 
-    if (msg = ''){
+    if (msg === ''){
         if (correo.trim() == '' || contrasena.trim() == ''){
             msg += 'El correo y la contraseña no pueden estar vacíos.'
-            return false
+            return msg;
         }
 
         if (contrasena.length < 8){
-            msg += 'La contraseña debe ser de 8 carácteres como mínimo.'
-            return false
+            msg += 'La contraseña debe ser de 8 caracteres como mínimo.'
+            return msg;
         }
     }
-    window.location.href = 'pagina.html'
-    return false
+
+    return msg;
 }
 
 function validarEmail(){
-    var email = document.getElementById('user').value
+    var email = document.querySelector('input[type="email"]').value
     var msg = ''
-    //user@gmail.com
-    email.charAt(0)
-    var dd = email.split('@')
-    //var dd = email.split('@') --> dd = ['u', 's', 'e', 'r', '@', 'g', 'm', 'a', 'i', 'l', '.', 'c', 'o', 'm']
-    //el correo debe incluir tan solo un @
+
     if (!email.includes('@')){
         msg += 'No incluye @'
     }
 
-    //el correo no puede empezar por el @
     if (email.charAt(0) == '@'){
         msg += '\nNo puede empezar por @'
     }
 
-    //el correo no puede tener @ en las tres últimas letras
-    for (let i = email.length - 3; i < email.length - 1; i++){
+    for (let i = email.length - 3; i < email.length; i++){
         if (email.charAt(i) == '@'){
-            msg += '\nNo puede tener @ en las tres ultimas letras'
+            msg += '\nNo puede tener @ en las tres últimas letras'
         }
     }
+
+    return msg;
+}
+
+function validarDatos(){
+    var nombre = document.getElementById('nombre').value
+    var apellido1 = document.getElementById('apellido1').value
+    var apellido2 = document.getElementById('apellido2').value
+    var dni = document.getElementById('dni').value
+    var msg = ''
+
+    if (nombre.trim() == '' || apellido1.trim() == '' || apellido2.trim() == '' || dni.trim() == ''){
+        msg += 'Todos los campos son obligatorios.\n'
+    }
+
+    if (!/^\d{8}[A-Z]$/.test(dni)){
+        msg += 'El DNI no es válido. Debe tener 8 números seguidos de una letra.'
+    }
+
+    if (msg !== ''){
+        alert(msg)
+        return false;
+    }
+
+    return true;
 }
