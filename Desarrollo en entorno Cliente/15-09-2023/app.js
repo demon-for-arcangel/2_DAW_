792a4bfddc8bdcc70c25095f984047fc3802e7e6
Marina Laguna <123579383+demon-for-arcangel@users.noreply.github.com>
2023-09-21 10:59:23 +0200

    Update app.js

--- Desarrollo en entorno Cliente/15-09-2023/app.js
+++ Desarrollo en entorno Cliente/15-09-2023/app.js
@@ -19,7 +19,7 @@ function acceder(){
 }
 
 function guardar(){
-    var validado = validar ()
+    var validado = validarDatos ()
     if (validado) {
         // Guardar los datos en el objeto persona
         persona.nombre = document.getElementById('nombre').value
@@ -29,14 +29,24 @@ function guardar(){
     }
 }
 
-function validar(){
+function validarLogin(){
     var correo = document.getElementById("correo").value
     var contrasena = document.getElementById("contrasena").value
 
     if (correo.trim() == "" || contrasena.trim() == ""){ //el método trim() elimina los espacios en blanco en ambos extremos del string
         alert('El correo y la contraseña no pueden estar vacíos')
     }
+
+    if (contrasena.length < 8){
+        alert('La contraseña debe ser de 8 carácteres como mínimo.')
+        return false;
+    }
+
+    window.location.href = "pagina.html";
+    return false;
 }
+
+function validarDatos(){}
 
 function limpiar(){
     document.getElementById('nombre').value = ''
