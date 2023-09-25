d24efbd028dced39c354620e189152f9437d7bd8
Marina <marinalaguna2004@gmail.com>
2023-09-25 08:46:12 +0200

    reducir código de la serpiente

--- Desarrollo en entorno Servidor/Serpiente/Nido.php
+++ Desarrollo en entorno Servidor/Serpiente/Nido.php
@@ -13,18 +13,4 @@ class Nido{
     public function cantidadSerpientes() {
         return count($this->serpientes);
     }    
-
-    public function simularUnAnio(){
-        foreach ($this -> serpientes as $serpiente){
-            if ($serpiente -> estaViva()){
-                $serpiente -> envejecer();
-                $serpiente -> serAtacada();
-            }
-        }
-    }
-
-    public function imprimirEstado(){
-        foreach ($this -> serpientes as $index => $serpiente){
-            echo "Serpiente $index: Edad: {$serpiente->getEdad()}, Anillas: " . count($serpiente->anillas) . ", Viva: " . ($serpiente->estaViva() ? 'Sí' : 'No') . "\n";        }
-    }
 }
