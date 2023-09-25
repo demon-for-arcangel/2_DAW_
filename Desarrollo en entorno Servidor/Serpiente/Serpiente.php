73cff0123642603b4f0090f192b7403e8a6ce579
Marina <marinalaguna2004@gmail.com>
2023-09-25 09:16:50 +0200

    añadir funciones a la serpiente

--- Desarrollo en entorno Servidor/Serpiente/Serpiente.php
+++ Desarrollo en entorno Servidor/Serpiente/Serpiente.php
@@ -18,4 +18,56 @@ class Serpiente{
     public function getEdad(){
         return $this -> edad;
     }
+
+    public function estaViva(){
+        return $this -> viva;
+    }
+
+    public function crecer(){
+        $colores = ['r', 'v', 'a'];
+        $colorAleatorio = $colores[array_rand($colores)];
+        $this -> anillas[] = new Anilla($colorAleatorio);
+    }
+
+    public function decrecer(){
+        if(count($this -> anillas) > 0){
+            array_pop($this -> anillas);
+        }else{
+            $this -> viva = false;
+        }
+    }
+
+    public function mudarPiel(){
+        foreach ($this -> anillas as $anilla){
+            $colores = ['r', 'v', 'a'];
+            $colorAleatorio = $colores[array_rand($colores)];
+            $anilla = new Anilla($colorAleatorio);
+        }
+    }
+
+    public function envejecer(){
+        $this -> edad++;
+        if ($this -> edad < 10){
+            $rand = rand(1, 10);
+            if ($rand <= 8){
+                $this -> crecer();
+            }else{
+                $this -> mudarPiel();
+            }
+        }else{
+            $rand = rand(1, 10);
+            if ($rand <= 9){
+                $this -> decrecer();
+            }else{
+                $this -> mudarPiel();
+            }
+        }
+    }
+
+    public function serAtacada(){
+        $rand = rand(1, 10);
+        if ($rand == 1){
+            $this -> viva = false;
+        }
+    }
 }
